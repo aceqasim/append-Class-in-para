51b0e4d4fc9901abf0e5b3cdb517591f03b3c906
aceqasim <m.kaxim21@gmail.com>
2025-07-15 22:17:13 +0500

    added css and all js code

---
+++ script.js
@@ -0,0 +1,15 @@
+// let btn = document.querySelector(".btn") 
+// let div = document.querySelector(".div") 
+// let  newBtn = document.createElement("button") 
+// newBtn.innerText="Click me"
+// console.log(newBtn)
+// div.after(newBtn)
+
+
+// // div.append(newBtn)
+// div.append("appended a btton")
+
+
+// question # 2
+let para = document.querySelector("p")
+para.classList.add("newClass")
